f2be49d6c4422bb87d8817193f04bc5a412a47c2
Masayuki Muto <muto@timedia.co.jp>
2016-08-11 21:31:33 +0900

    Generate StartCore

---
+++ StartCore/Controllers/StartController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Web.Http;
+
+namespace StartCore.Controllers
+{
+    public class StartController : ApiController
+    {
+        [HttpPost]
+        public void Post([FromBody]string[] args)
+        {
+            switch (args.Length)
+            {
+                case 0:
+                    return;
+
+                case 1:
+                    Process.Start(args[0]);
+                    return;
+
+                default:
+                    Process.Start(args[0], string.Join(" ", args.Skip(1)));
+                    return;
+            }
+        }
+    }
+}
