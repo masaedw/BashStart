7bf4cc963f2c4fabf58756ce8942b37920b2ee5b
Masayuki Muto <muto@timedia.co.jp>
2016-08-11 23:28:59 +0900

    Show Error dialog

--- StartCore/Controllers/StartController.cs
+++ StartCore/Controllers/StartController.cs
@@ -1,6 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Web.Http;
+using System.Windows.Forms;
 
 namespace StartCore.Controllers
 {
@@ -9,18 +11,25 @@ namespace StartCore.Controllers
         [HttpPost]
         public void Post([FromBody]string[] args)
         {
-            switch (args.Length)
+            try
             {
-                case 0:
-                    return;
+                switch (args.Length)
+                {
+                    case 0:
+                        return;
 
-                case 1:
-                    Process.Start(args[0]);
-                    return;
+                    case 1:
+                        Process.Start(args[0]);
+                        return;
 
-                default:
-                    Process.Start(args[0], string.Join(" ", args.Skip(1)));
-                    return;
+                    default:
+                        Process.Start(args[0], string.Join(" ", args.Skip(1)));
+                        return;
+                }
+            }
+            catch (Win32Exception e)
+            {
+                MessageBox.Show(e.Message, args.FirstOrDefault() ?? "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
